e7d4fb390ef00bafed7d1b2c75573a18a0041dae
ANTOINE LALA <antoine.lala@iut.local>
2022-10-26 14:16:06 +0200

    avancement base de données

--- ServeurC.py
+++ ServeurC.py
@@ -1,19 +1,31 @@
 import socket
+import sqlite3
+import os
 
-class Communication:
+cheminBd = 'Bd_babillage.db'
 
-    def _init_(self, comm, coord):
-        self.comm = comm
-        self.coord = coord
+def initDb():
+    if (not(os.path.exists(cheminBd) and os.path.isfile(cheminBd))):
+        connectionBd = sqlite3.connect(cheminBd)
+        cursor = connectionBd.cursor()
+        cursor.execute(""" CREATE TABLE clefs (nom TEXT, clef TEXT, CONSTRAINT pk_clefs PRIMARY KEY (nom)) """)
+        connectionBd.close()
+
+def ajout(nom,cle):
+    connectionBd = sqlite3.connect(cheminBd)
+    cursor = connectionBd.cursor()
+    cursor.execute("INSERT INTO clefs VALUES (?,?)", (nom,cle))
+    connectionBd.commit()
+    connectionBd.close()
+
+initDb()
+ajout('A','ABC')
+
+connectionBd = sqlite3.connect(cheminBd)
+cursor = connectionBd.cursor()
+test = cursor.execute(" SELECT * FROM clefs")
+print (test.fetchall())
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 coordServeur = ('127.0.0.1',12345)
 s.bind(coordServeur)
-s.listen(2)
-
-(commA,coordA) = s.accept()
-A = Communication(commA, coordA)
-
-(commB, coordB) = s.accept()
-B = Communication(commB, coordB)
-
