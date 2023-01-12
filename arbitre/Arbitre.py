15a578733e42e0ecc96326522987d62a28ece71a
AstroNey <l.nicolas02@laposte.net>
2023-01-12 11:11:20 +0100

    tirage aleatoire du PORT si 5000 occupé

--- arbitre/Arbitre.py
+++ arbitre/Arbitre.py
@@ -5,29 +5,30 @@ import signal
 import threading as th
 
 
-listening_ip: str = ''  # '' = all interfaces
-listening_port: int = 5000
-
-def create_socket():
+def create_socket(ip: str, port: int) -> tuple[socket, int]:
     """
     Créé un socket UDP et la bind sur l'adresse et le port d'écoute
     :return: socket
     """
-    sck = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        sck.bind((listening_ip, listening_port))
-    except socket.error as e:
-        print('Erreur lors de la création du socket : ', e, '\n Le port d\'écoute est-t-il déjà utilisé ?')
-        return None
 
-    if listening_ip == '':
+    sck = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ok = False
+    while not ok:
+        try:
+            sck.bind((ip, port))
+            ok = True
+        except socket.error as e:
+            port = 0
+            print('Erreur lors de la création du socket : ', e, '\n Le port d\'écoute est-t-il déjà utilisé ?')
+
+    if ip == '':
         print('WARN : Le serveur écoute sur toutes les interfaces réseau !')
         # Liste de toutes les ip sur lesquelles le serveur écoute
         for ip in socket.gethostbyname_ex(socket.gethostname())[2]:
-            print(' - ', ip + ':' + str(listening_port))
+            print(' - ', ip + ':' + str(sck.getsockname()[1]))
     else:
-        print("Serveur démarré sur", socket.gethostbyname(socket.gethostname()) + ':' + str(listening_port))
-    return sck
+        print("Serveur démarré sur", socket.gethostbyname(socket.gethostname()) + ':' + str(sck.getsockname()[1]))
+    return sck, sck.getsockname()[1]
 
 
 def stop_server(signum, frame):
@@ -45,10 +46,13 @@ def stop_server(signum, frame):
 
 
 if __name__ == "__main__":
+    listening_ip: str = ''  # '' = all interfaces
+    listening_port: int = 5000
+
     print('Démarrage du serveur... Appuyez sur Ctrl-C pour arrêter le serveur')
     signal.signal(signal.SIGINT, stop_server)  # Ctrl-C pour arrêter le serveur
 
-    sck = create_socket()
+    sck, listening_port = create_socket(listening_ip, listening_port)
     if sck is not None:
         Database.init_db()
         recvthread = th.Thread(target=Communicate.recv, args=(sck,))
